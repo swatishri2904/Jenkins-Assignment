0b0af16645ea4f13025646423d84fc982400c83c
SwatiSrivastava <swati.srivastava@nagarro.com>
2024-08-11 21:37:02 +0530

    changes

--- src/main/java/jenkinsassignment/Calculator.java
+++ src/main/java/jenkinsassignment/Calculator.java
@@ -2,8 +2,11 @@ package jenkinsassignment;
 
 public class Calculator {
 
-	public Calculator() {
+	
+		public int add(int a, int b) {
+	        return a + b;
+	    }
 		// TODO Auto-generated constructor stub
 	}
 
-}
+
